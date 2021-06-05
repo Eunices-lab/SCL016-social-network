9144d99f5c8cb851d82e3e6a20d2e84734aec84b
Eunices <77020318+Naaho@users.noreply.github.com>
2021-06-05 18:41:33 -0400

    ajustes css

--- src/lib/view/templateRegister.js
+++ src/lib/view/templateRegister.js
@@ -3,18 +3,20 @@ export const register = () => {
 
     const viewRegister = /* html*/ `
     <div id="contenedorRegistro" class="contenedorRegistro">
-    <h3>¿No te has registrado aún?</h3>
+    <h3 id="leyend1">¿No te has registrado aún?</h3>
 
-    <p>Ingresa al formulario y registra tu usuario</p>
+    <p id="leyend3">Ingresa al formulario y registra tu usuario</p>
 
-    </div>
-    <div id="contenedorFormRegister">
     <form class="formularioRegistro" id="formularioRegistro">
-    <h2>Registrarse</h2>
+    <h2 id="registro">Registrarse</h2>
     <input type="text" placeholder="Usuario" id="user">
+    <br/>
     <input type="email" placeholder="Correo Electronico" id="emailReg" name ="email">
+    <br/>
     <input type="password" placeholder="Contraseña" id="passReg" name ="password">
+    <br/>
     <input type="button" id="btnRegistro" value="Registrarse">
+    <br/>
     </form>
     </div>
     `
